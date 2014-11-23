366f36948471729180956d9a3d95513c8f707144
Devin-Moore-CpE <devinmoore89@gmail.com>
2014-11-22 22:15:32 -0800

    Prototyping control blocks

--- source/Design/control_hazard_unit.sv
+++ source/Design/control_hazard_unit.sv
@@ -1,9 +1,20 @@
 module hazard_detection_unit(
+	input wire  			r0_en,
+	input wire		[3:0]	opcode,
+	input wire		[3:0]	s2_opcode,
+	input wire		[3:0]	s3_opcode,
 
-	);
+	input wire		[3:0]	r1,
+	input wire		[3:0]	r2,
+	input wire		[3:0]	s2_r1,
+	input wire		[3:0]	s3_r1,
 
+	output logic	[10:0]	haz,
+	output logic 			stall
+);
 
-
+always_comb 
+	assert($oneshot(haz));
 
 
 
--- source/Design/control_main.sv
+++ source/Design/control_main.sv
@@ -1,3 +1,14 @@
+typedef enum logic[3:0]{
+		ARITHM		= 4'b0000,
+		LW			= 4'b1000,
+		SW			= 4'b1011,
+        BLT 		= 4'b0100,
+        BGT	     	= 4'b0101,
+        BE 			= 4'b0110,
+        JMP  		= 4'b1100,
+        HALT 		= 4'b1111      
+} opcode_t;
+
 module control_main(
 	input opcode_t 		opcode,
 	input wire 			div0,
@@ -13,9 +24,27 @@ module control_main(
 	output logic 		se_imm_a
 );
 
+	always_comb begin
 
 
+		case (opcode)
+			ARITHM:
 
+			LW:
 
+			SW:
 
+			BLT:
+
+			BGT:
+
+			BE:
+
+			JMP:
+
+			HALT: 
+
+			default: 			// Exception
+		endcase
+	end
 endmodule
