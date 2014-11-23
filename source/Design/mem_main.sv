1bd591fcc82516958cdb7bb63a0764cda8641b83
Devin-Moore-CpE <devinmoore89@gmail.com>
2014-11-22 16:16:34 -0800

    More memory development.

--- source/Design/mem_main.sv
+++ source/Design/mem_main.sv
@@ -9,6 +9,7 @@ module mem_main(
 	input wire 			clk,
 	input wire 			halt_sys,
 
+
 	input wire 			write_en,
 	input wire 	[15:0] 	address,
 	input wire 	[15:0] 	write_data,
@@ -17,16 +18,16 @@ module mem_main(
 );
 
 
-	logic 	[15:0] 	memory 	[65535:0];	// Memory block. 16 bit address with 16 bit data
+	logic 	[15:0][65535:0]	memory;	// Memory block. 16 bit address with 16 bit data
 	
 	assign data_out = memory[address];	// Always read the data from the address
 
 	always_ff@ (posedge clk or posedge rst) begin: mem_main_flop
 		if (rst) begin		
-			memory <= '0;// If rst is asserted, we want to clear the flops
+			memory <= 0;// If rst is asserted, we want to clear the flops
 		end 
 		else begin
-			if(halt_sys or stall or !write_en) 
+			if(halt_sys || !write_en) 
 				memory[address] <= memory[address]; // Stay the same value. System is halted.
 			else 
 				memory[address] <= write_data; 	// Flop the input
--- source/Design/mem_register.sv
+++ source/Design/mem_register.sv
@@ -3,27 +3,43 @@ module register_file(
 	input wire 			clk,
 	input wire 			halt_sys,
 
-	input wire 			write_en,
-	input wire 	[15:0] 	address,
-	input wire 	[15:0] 	write_data,
+	input wire 			R0_read,
+  	input wire 	[3:0]	ra1,
+  	input wire 	[3:0]	ra2,
 
-	output logic[15:0]	data_out
+	input wire 			write_en,
+	input wire 			R0_en,
+	input wire 	[3:0] 	write_address,
+	input wire 	[31:0] 	write_data,
+
+	output logic[15:0]	rd1,
+	output logic[15:0] 	rd2
 );
 
+	wire 	[15:0]			write_data_high;
+	wire 	[15:0]			write_data_low;
 
-	logic 	[15:0] 	memory 	[65535:0];	// Memory block. 16 bit address with 16 bit data
+	logic 	[15:0][15:0] 	registers;	// Memory block. 16 bit address with 16 bit data
 	
-	assign data_out = memory[address];	// Always read the data from the address
+	assign {write_data_high, write_data_low} = write_data; 	// Split the input data
+															// into two words
+	assign rd1 = registers[ra1];	// Always read the data from the address
+	// If a branch instruction(R0_read is high) then R0 contents are output at rd2
+	assign rd2 = (Ro_read) ? registers[0] : registers[ra2];	
 
-	always_ff@ (posedge clk or posedge rst) begin: mem_main_flop
+
+	always_ff@ (posedge clk or posedge rst) begin: mem_reg_flop
 		if (rst) begin		
-			memory <= '0;// If rst is asserted, we want to clear the flops
+			registers <= 'd0;// If rst is asserted, we want to clear the flops
 		end 
 		else begin
-			if(halt_sys or stall or !write_en) 
-				memory[address] <= memory[address]; // Stay the same value. System is halted.
-			else 
-				memory[address] <= write_data; 	// Flop the input
+			if(halt_sys || !write_en) 
+				registers[write_address] <= registers[write_address]; // Stay the same value. System is halted.
+			else begin // Write data to reg, and write top 16 bits to R0 if R0_en is high 
+				if (R0_en)
+					registers[0] <= write_data_high;
+				registers[write_address] <= write_data_low; 	// Flop the input
+			end
 		end
 	end
 endmodule
